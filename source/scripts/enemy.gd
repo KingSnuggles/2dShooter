d0dc95bb812016557afff1016803e27b39b823d8
Allen Hartley <allenhartley606@gmail.com>
2023-08-04 08:36:22 +0200

    enemy movement

--- source/scripts/enemy.gd
+++ source/scripts/enemy.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 var enemy_health:int = 40
 var enemy_speed:int = 50
 var player
@@ -11,13 +10,9 @@ func _ready():
 	
 	player = get_node("../player")
 	
-func _process(_delta):
-	pass
+func _process(delta):
+	position = position.move_toward(player.position,enemy_speed * delta)
 	
-	
-func _physics_process(delta):
-	print(player.position)
-	position += transform.x * enemy_speed * delta
 
 func handle_damage_enemy(damage:int = 10):
 	enemy_health = enemy_health - damage
